9565cc57ec5deaecb5f7e43a996194d50e1c4836
menyagah <menyagah27@gmail.com>
2024-01-31 16:10:29 +0300

    Added model boilerplate

---
+++ app/Http/Controllers/AccomodationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAccomodationRequest;
+use App\Http\Requests\UpdateAccomodationRequest;
+use App\Models\Accomodation;
+
+class AccomodationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAccomodationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Accomodation $accomodation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAccomodationRequest $request, Accomodation $accomodation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Accomodation $accomodation)
+    {
+        //
+    }
+}
