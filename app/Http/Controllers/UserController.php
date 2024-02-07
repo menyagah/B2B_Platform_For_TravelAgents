bb06e39190ddeb1cad59580646378385a637f90c
menyagah <menyagah27@gmail.com>
2024-02-07 21:11:23 +0300

    Refactored User controller to return using user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
 use App\Models\Accommodation;
 use App\Models\Booking;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -16,43 +18,47 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return ResourceCollection
      */
     public function index()
     {
         $users = User::query()->get();
-        return new JsonResponse([
-            'data'=> $users
-        ]);
+        return UserResource::collection($users);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return UserResource
      */
-    public function store(StoreUserRequest $request)
+    public function store(Request $request)
     {
         $created = User::query()->create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password
         ]);
-        return new JsonResponse([
-            'data' => $created
-        ]);
+        return new UserResource($created);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param User $user
+     * @return UserResource
      */
     public function show(User $user)
     {
-        return new \Illuminate\Http\JsonResponse([
-            'data' => $user
-        ]);
+        return new UserResource($user);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param User $user
+     * @return UserResource | JsonResponse
      */
     public function update(Request $request, User $user)
     {
@@ -66,13 +72,14 @@ class UserController extends Controller
                 ]
             ], status: 400);
         }
-        return new JsonResponse([
-            'data' => $updated
-        ]);
+        return new UserResource($updated);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param User $user
+     * @return JsonResponse
      */
     public function destroy(User $user)
     {
