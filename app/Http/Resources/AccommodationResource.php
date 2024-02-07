6b92175e419185dfa3964cdfed15c7a6ec3752b6
menyagah <menyagah27@gmail.com>
2024-02-07 20:36:37 +0300

    Returned an array on the accommodation resource controller

--- app/Http/Resources/AccommodationResource.php
+++ app/Http/Resources/AccommodationResource.php
@@ -14,6 +14,13 @@ class AccommodationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'standard_rack_rate' => $this->standard_rack_rate,
+            'created_at'=> $this->created_at,
+            'updated_at'=> $this->updated_at
+        ];
     }
 }
