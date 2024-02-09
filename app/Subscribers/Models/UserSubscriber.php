14a2226f243092156d349cb15c91300e47f34b75
menyagah <menyagah27@gmail.com>
2024-02-09 12:02:22 +0300

    Create a subscriber directory and class, add logic to it

--- app/Subscribers/Models/UserSubscriber.php
+++ app/Subscribers/Models/UserSubscriber.php
@@ -2,7 +2,14 @@
 
 namespace App\Subscribers\Models;
 
+use App\Events\Models\Users\UserCreated;
+use App\Listeners\SendWelcomeEmail;
+use Illuminate\Events\Dispatcher;
+
 class UserSubscriber
 {
-
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(UserCreated::class, SendWelcomeEmail::class);
+    }
 }
