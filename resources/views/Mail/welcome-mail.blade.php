b80d81fbd0965a6c6ecc5a4111b70dbb4c7f8db4
menyagah <menyagah27@gmail.com>
2024-02-13 12:34:32 +0300

    Create email template file

---
+++ resources/views/Mail/welcome-mail.blade.php
@@ -0,0 +1,18 @@
+<x-mail::message >
+    # Welcome {{ $name }}!!
+
+    Congratulations on joining the x-team. We are pleased to have you onboard!
+
+    Thanks.
+    X-dev team!
+<x-mail::button :url="''">
+    click Here for Onboarding
+</x-mail::button>
+</x-mail::message>
+
+
+
+
+
+
+
